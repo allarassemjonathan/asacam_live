0a41f505059053bf480dbd9195e69a3f8d5b0142
Jonathan Allarassem <jonat@5CG024DGD4.gcc.edu>
2025-07-04 14:29:32 -0400

    timeout issue v0.2 -- added monkey_patch()

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 from flask_mail import Mail, Message
